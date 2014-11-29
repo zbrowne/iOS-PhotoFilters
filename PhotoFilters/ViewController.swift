4bf8b702a9f70a5c2ca2259fb66766091b147809
Zachary Browne <browne.zachary@gmail.com>
2014-11-28 22:07:19 -0800

    Initial Commit

---
+++ PhotoFilters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoFilters
+//
+//  Created by Zachary Browne on 11/28/14.
+//  Copyright (c) 2014 Zachary Browne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
